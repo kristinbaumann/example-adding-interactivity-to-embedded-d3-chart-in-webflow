07b5b7e3958a641abe0982831d73d729c616c4f6
Kristin Baumann <hello@kristin-baumann.com>
2025-02-11 10:02:17 +0100

    Improved

--- script.js
+++ script.js
@@ -7,89 +7,119 @@ const locations = [
     id: "sfo",
     text: "San Francisco",
     dataFile: "sfo-temperature-data.csv",
+    pathColor: "steelblue",
   },
   {
     id: "ord",
     text: "Chicago",
     dataFile: "ord-temperature-data.csv",
+    pathColor: "tomato",
   },
 ];
 
-// populate the chart dropdown div
-const dropdownContainer = document.querySelector("#chart-dropdown");
-const dropdownElement = document.createElement("select");
-locations.forEach((location) => {
-  let option = document.createElement("option");
-  option.text = location.text;
-  option.value = location.id;
-  dropdownElement.add(option);
-});
-if (dropdownContainer) dropdownContainer.appendChild(dropdownElement);
+const initialLocationIndex = 0;
 
-// add event listener for dropdown
-dropdownContainer.addEventListener("change", (event) => {
-  console.log("Dropdown changed to", event.target.value);
-});
+function populateDropdown(allData) {
+  // populate the chart dropdown div
+  const dropdownContainer = document.querySelector("#chart-dropdown");
+  const dropdownElement = document.createElement("select");
+  locations.forEach((location) => {
+    let option = document.createElement("option");
+    option.text = location.text;
+    option.value = location.id;
+    dropdownElement.add(option);
+  });
+  if (dropdownContainer) dropdownContainer.appendChild(dropdownElement);
 
-const dataURL =
-  "https://kristinbaumann.github.io/example-adding-interactivity-to-embedded-d3-chart-in-webflow/data/sfo-temperature-data.csv";
+  // add event listener for dropdown
+  dropdownContainer.addEventListener("change", (event) => {
+    console.log("Dropdown changed to", event.target.value);
+    const newData = allData[event.target.value];
+    updateViz(newData, event.target.value);
+  });
+}
 
-// load data
-d3.csv(dataURL).then((data) => {
-  const groupedData = d3
-    .groups(
-      data,
-      ({ DATE }) =>
-        new Date(
-          Date.UTC(
-            2000,
-            new Date(DATE).getUTCMonth(),
-            new Date(DATE).getUTCDate()
-          )
-        ) // group by day of year
-    )
-    .sort(([a], [b]) => d3.ascending(a, b)) // sort chronologically
-    .map(([date, v]) => ({
-      date,
-      avg: d3.mean(v, (d) => d.TAVG || NaN),
-      min: d3.mean(v, (d) => d.TMIN || NaN),
-      max: d3.mean(v, (d) => d.TMAX || NaN),
-      minmin: d3.min(v, (d) => d.TMIN || NaN),
-      maxmax: d3.max(v, (d) => d.TMAX || NaN),
-    }));
+// load data from both URLs
+async function loadData() {
+  function getDataURL(location) {
+    return `https://kristinbaumann.github.io/example-adding-interactivity-to-embedded-d3-chart-in-webflow/data/${location}-temperature-data.csv`;
+  }
+  return Promise.all([
+    d3.csv(getDataURL("sfo"), d3.autoType),
+    d3.csv(getDataURL("ord"), d3.autoType),
+  ]).then((datasets) => {
+    const [dataSFO, dataORD] = datasets;
 
-  createViz(groupedData);
-});
+    const processData = (data) => {
+      return d3
+        .groups(
+          data,
+          ({ DATE }) =>
+            new Date(
+              Date.UTC(
+                2000,
+                new Date(DATE).getUTCMonth(),
+                new Date(DATE).getUTCDate()
+              )
+            ) // group by day of year
+        )
+        .sort(([a], [b]) => d3.ascending(a, b)) // sort chronologically
+        .map(([date, v]) => ({
+          date,
+          avg: d3.mean(v, (d) => d.TAVG || NaN),
+          min: d3.mean(v, (d) => d.TMIN || NaN),
+          max: d3.mean(v, (d) => d.TMAX || NaN),
+        }));
+    };
+    const groupedData = {
+      sfo: processData(dataSFO),
+      ord: processData(dataORD),
+    };
+    return groupedData;
+  });
+}
 
-function createViz(data) {
-  // dimensions
-  const width = 900;
-  const height = width;
-  const margin = 10;
-  const innerRadius = width / 5;
-  const outerRadius = width / 2 - margin;
+(async function () {
+  const allData = await loadData();
+  populateDropdown(allData);
+  drawViz(allData);
+})();
 
-  // scales
-  const x = d3
-    .scaleUtc()
-    .domain([new Date("2000-01-01"), new Date("2001-01-01") - 1])
-    .range([0, 2 * Math.PI]);
+// dimensions
+const width = 900;
+const height = width;
+const margin = 10;
+const innerRadius = width / 5;
+const outerRadius = width / 2 - margin;
 
-  const y = d3
-    .scaleRadial()
-    .domain([d3.min(data, (d) => d.minmin), d3.max(data, (d) => d.maxmax)])
-    .range([innerRadius, outerRadius]);
+// scales
+const x = d3
+  .scaleUtc()
+  .domain([new Date("2000-01-01"), new Date("2001-01-01") - 1])
+  .range([0, 2 * Math.PI]);
 
-  // line and area generators
-  const line = d3
-    .lineRadial()
-    .curve(d3.curveLinearClosed)
-    .angle((d) => x(d.date));
+const y = d3
+  .scaleRadial()
+  // .domain([d3.min(data, (d) => d.minmin), d3.max(data, (d) => d.maxmax)])
+  .range([innerRadius, outerRadius]);
 
-  const area = d3
-    .areaRadial()
-    .curve(d3.curveLinearClosed)
-    .angle((d) => x(d.date));
+// line and area generators
+const line = d3
+  .lineRadial()
+  .curve(d3.curveLinearClosed)
+  .angle((d) => x(d.date));
+
+const area = d3
+  .areaRadial()
+  .curve(d3.curveLinearClosed)
+  .angle((d) => x(d.date));
+
+function drawViz(groupedData) {
+  console.log("groupedData", groupedData);
+  const data = groupedData[locations[initialLocationIndex].id];
+  console.log("drawn data", data);
+
+  y.domain([d3.min(data, (d) => d.min), d3.max(data, (d) => d.max)]);
 
   // create svg
   const svg = d3
@@ -103,17 +133,19 @@ function createViz(data) {
     .attr("stroke-linecap", "round");
 
   // draw background areas
+  // svg
+  //   .append("path")
+  //   .attr("class", "big-area")
+  //   .attr("fill", "lightsteelblue")
+  //   .attr("fill-opacity", 0.2)
+  //   .attr(
+  //     "d",
+  //     area.innerRadius((d) => y(d.minmin)).outerRadius((d) => y(d.maxmax))(data)
+  //   );
   svg
     .append("path")
-    .attr("fill", "lightsteelblue")
-    .attr("fill-opacity", 0.2)
-    .attr(
-      "d",
-      area.innerRadius((d) => y(d.minmin)).outerRadius((d) => y(d.maxmax))(data)
-    );
-  svg
-    .append("path")
-    .attr("fill", "steelblue")
+    .attr("class", "min-max-area")
+    .attr("fill", locations[initialLocationIndex].pathColor)
     .attr("fill-opacity", 0.2)
     .attr(
       "d",
@@ -122,8 +154,9 @@ function createViz(data) {
   // draw average line
   svg
     .append("path")
+    .attr("class", "avg-line")
     .attr("fill", "none")
-    .attr("stroke", "steelblue")
+    .attr("stroke", locations[initialLocationIndex].pathColor)
     .attr("stroke-width", 1.5)
     .attr("d", line.radius((d) => y(d.avg))(data));
 
@@ -220,4 +253,23 @@ function createViz(data) {
     .text("and recorded extremes in San Francisco")
     .attr("dy", "0em");
   headlineGroup.append("text").text("from 1999–2018").attr("dy", "1em");
+}
+
+function updateViz(newData, locationId) {
+  console.log("Updating the viz");
+
+  let svg = d3.select("#chart-container").transition();
+
+  // update the y scale
+  y.domain([d3.min(newData, (d) => d.min), d3.max(newData, (d) => d.max)]);
+
+  // change the line
+  svg
+    .select(".avg-line")
+    .duration(750)
+    .attr("stroke", locations.find((loc) => loc.id === locationId).pathColor)
+    .attr("d", line.radius((d) => y(d.avg))(newData));
+
+  // change the big area
+  // svg.select(".big-area").duration(750).attr("d", area(newData));
 }
